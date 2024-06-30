569f4aba374c338d79f1b21ea9fd37d0e4cc7a4c
Ryugo SATO <sugardragon5@g.ecc.u-tokyo.ac.jp>
2024-06-30 20:16:44 +0900

    fix bug

--- client.py
+++ client.py
@@ -3,14 +3,16 @@ import subprocess
 
 app = Flask(__name__)
 
-server_name = 0
+server_name = 'None'
 
 @app.route('/')
 def hello_world():
     return f'Hello from {server_name}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
     proc = subprocess.Popen('whoami', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = proc.communicate()
-    server_name = stdout
+    server_name = stdout.decode('utf-8')
+    print(f'I\'m {server_name}')
+    app.run(host='0.0.0.0',port=5000)
+
--- main.py
+++ main.py
@@ -5,25 +5,17 @@ app = Flask(__name__)
 
 # バックエンドサーバーのリスト
 servers = [
-    'http://127.0.0.1:5001',
-    'http://127.0.0.1:5002'
+    'http://127.0.0.1:5000'
 ]
 current_server = 0
-
-async def request_server(server, path):
-    try:
-        response = requests.get(server + path)
-        return response.text
-    except requests.exceptions.RequestException as e:
-        return str(e), 500
 
 @app.route('/')
 def load_balancer():
     global current_server
     try:
-        response = request_server(servers[current_server], request.path)
+        response = requests.get(servers[current_server])
         current_server = (current_server + 1) % len(servers)
-        return response
+        return response.text
     except requests.exceptions.RequestException as e:
         return str(e), 500
 
